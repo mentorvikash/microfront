ceb349f4e97999a08d15405f1c6d31119919dce9
mentorvikash <mentorvikash@gmail.com>
2025-12-02 22:53:49 +0530

    add teacher component

--- teacher/src/TeacherApp.jsx
+++ teacher/src/TeacherApp.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function TeacherApp() {
+  const [classes, setClasses] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:4001/classes")
+      .then(res => res.json())
+      .then(date => setClasses)
+      .catch(() => setClasses([{ name: "No backend" }]))
+
+
+  }, [])
+
   return (
-    <div>TeacherApp</div>
+    <div>
+      <h1>
+        Teacher Micro Service
+      </h1>
+      <p>Classes from backend: </p>
+      <ul>
+        {classes.map((c, i) => <li key={i} >{c.name}</li>)}
+      </ul>
+    </div>
   )
 }
 
